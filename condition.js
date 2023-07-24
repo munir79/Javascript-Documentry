4b59bc96074f70690031a9b4a8745aff4d48bda4
munir79 <jakirhossainmunir79@gmail.com>
2023-07-24 12:14:29 +0600

    grade

--- condition.js
+++ condition.js
@@ -30,3 +30,36 @@ else {
 if(isgraduated===true || wellInProgramming==true  ){
     console.log(" ok for remote job");
 }
+
+// grade problem in js
+
+var marks=120;
+ if(marks>100){
+    console.log('your marks dosent exist');
+}
+else if(marks>=90){
+    console.log('your Grade is : A+');
+}
+else if(marks>=80){
+    console.log("your grade is :A+");
+}
+else if(marks>=70){
+    console.log("your grade is:A");
+}
+else if(marks>=60){
+    console.log("your grade id :A-");
+}
+else if(marks>=50){
+    console.log("your grade is:B");
+}
+else if(marks>=40){
+    console.log(" your grade is :c");
+
+}
+else if(marks>=33){
+    console.log("Your grade is :D");
+}
+else if(marks<33){
+    console.log("your grade is:F");
+}
+
