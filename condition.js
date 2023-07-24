c5bfeda571b52eb4e21a28dd2d78f3feb2109349
munir79 <jakirhossainmunir79@gmail.com>
2023-07-24 11:55:47 +0600

    condition2

--- condition.js
+++ condition.js
@@ -1,4 +1,9 @@
 
+// simple note
+// 1. assaign :- " = "
+// 2.is eqaual :- " ==  ,==="
+
+
 //1. simple if else
 
 var mybadget=500000;
@@ -8,4 +13,20 @@ if(mybadget>iphonePrice){
 }
 else{
     console.log("sorry you have not enough budget");
+}
+
+// 2. 
+
+var isgraduated=false ;
+var wellInProgramming=true;
+if(isgraduated==true && wellInProgramming==true){
+    console.log('condition in ok for job');
+}
+else {
+    console.log('not ok for government job');
+
+}
+
+if(isgraduated===true || wellInProgramming==true  ){
+    console.log(" ok for remote job");
 }
