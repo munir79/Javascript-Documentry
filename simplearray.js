3b25bdbf14f211976b0ef38d0e47f9b75c226345
munir79 <jakirhossainmunir79@gmail.com>
2023-07-24 10:55:04 +0600

    simplearray1

---
+++ simplearray.js
@@ -0,0 +1,14 @@
+//1. declare javascript
+var age=[20,30,40,50];
+console.log(age);
+// 2.string assign in array
+var name=['akas','kamal','rahim','rahman'];
+
+// 3.array length 
+console.log(age.length);
+
+//4. get element by index
+console.log(age[3]);
+var element =age[3];
+console.log(element);
+
