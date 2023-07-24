1fe525cff0e05ea4a5a6388aad5fa61a5e080576
munir79 <jakirhossainmunir79@gmail.com>
2023-07-24 11:38:21 +0600

    condition1

---
+++ condition.js
@@ -0,0 +1,11 @@
+
+//1. simple if else
+
+var mybadget=500000;
+iphonePrice=100000;
+if(mybadget>iphonePrice){
+    console.log("you are able to buy i phone");
+}
+else{
+    console.log("sorry you have not enough budget");
+}
