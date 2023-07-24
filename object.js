586c1494ea5cbcca1d6682ddf9c8e7e86b89d04b
munir79 <jakirhossainmunir79@gmail.com>
2023-07-24 17:22:12 +0600

    object2

--- object.js
+++ object.js
@@ -8,3 +8,16 @@ var mobile={
 }
 var name1=mobile.name;
 console.log(name1);
+console.log(mobile);
+//2.change value in object
+
+mobile.price='20k';
+console.log(mobile);
+// properties of objects are : name, price 
+
+// important
+var properties=Object.keys(mobile);
+console.log(properties);
+
+var values=Object.values(mobile);
+console.log(values);
