43a528bff3f350dca9964d488155185ca58deaeb
munir79 <jakirhossainmunir79@gmail.com>
2023-07-24 20:20:11 +0600

    loop

--- object.js
+++ object.js
@@ -1,23 +1,41 @@
-//1. Declare object
+// //1. Declare object
 
-var mobile={
-    name:'samsung',
-    price:'10k',
-    camera:"50pixel",
-    battery:"5000 A"
+// var mobile={
+//     name:'samsung',
+//     price:'10k',
+//     camera:"50pixel",
+//     battery:"5000 A"
+// }
+// var name1=mobile.name;
+// console.log(name1);
+// console.log(mobile);
+// //2.change value in object
+
+// mobile.price='20k';
+// console.log(mobile);
+// // properties of objects are : name, price 
+
+// // important
+// var properties=Object.keys(mobile);
+// console.log(properties);
+
+// var values=Object.values(mobile);
+// console.log(values);
+
+//3. loop in object
+
+var items={
+    mobile:3,
+    leptop:4,
+    watch:7,
+    pen:10,
+    car:10
 }
-var name1=mobile.name;
-console.log(name1);
-console.log(mobile);
-//2.change value in object
 
-mobile.price='20k';
-console.log(mobile);
-// properties of objects are : name, price 
+var propName=Object.keys(items);
 
-// important
-var properties=Object.keys(mobile);
-console.log(properties);
-
-var values=Object.values(mobile);
-console.log(values);
+for (var i=0;i<propName.length;i++){
+    var prop=propName[i];
+   var  propvalue=items[prop];
+   console.log(prop,propvalue);
+}
