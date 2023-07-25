e1f58b0f7c91cb49773fdda352a897e9f4d38e41
munir79 <jakirhossainmunir79@gmail.com>
2023-07-25 10:30:30 +0600

    string

--- object.js
+++ object.js
@@ -1,28 +1,28 @@
-// //1. Declare object
+//1. Declare object
 
-// var mobile={
-//     name:'samsung',
-//     price:'10k',
-//     camera:"50pixel",
-//     battery:"5000 A"
-// }
-// var name1=mobile.name;
-// console.log(name1);
-// console.log(mobile);
-// //2.change value in object
+var mobile={
+    name:'samsung',
+    price:'10k',
+    camera:"50pixel",
+    battery:"5000 A"
+}
+var name1=mobile.name;
+console.log(name1);
+console.log(mobile);
+//2.change value in object
 
-// mobile.price='20k';
-// console.log(mobile);
-// // properties of objects are : name, price 
+mobile.price='20k';
+console.log(mobile);
+// properties of objects are : name, price 
 
-// // important
-// var properties=Object.keys(mobile);
-// console.log(properties);
+// important
+var properties=Object.keys(mobile);
+console.log(properties);
 
-// var values=Object.values(mobile);
-// console.log(values);
+var values=Object.values(mobile);
+console.log(values);
 
-//3. loop in object
+3. loop in object
 
 var items={
     mobile:3,
---
+++ string.js
@@ -0,0 +1,24 @@
+// 1. to lowercase
+
+const userName='BlackPink';
+const userInput='blackpink';
+
+if(userName.toLowerCase()==userInput.toLowerCase()){
+    console.log("valid User");
+
+}
+else{
+    console.log("invalid User");
+}
+
+//2. to uppercase
+
+const username1='Blackpink';
+const userinput1='blackpink';
+
+if(username1.toLowerCase()==userinput1.toUpperCase()){
+    console.log('valid User');
+}
+else{
+    console.log('invalid user');
+}
