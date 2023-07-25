4c1b4066ca5435073d4a135411ce6f8169e59c99
munir79 <jakirhossainmunir79@gmail.com>
2023-07-25 11:50:09 +0600

    math

---
+++ math.js
@@ -0,0 +1,25 @@
+
+// 1. math.pow
+const result=Math.pow(3,8);
+console.log(result);
+
+//2. math.abs
+const a=20;
+const b=40;
+const gap=a-b;
+const result1=Math.abs(gap);
+console.log(result1);
+
+//3. math.round
+const c=2.6777;
+const round=Math.round(c);
+console.log(round);
+//4. math.ceil, floor
+const cell=Math.ceil(c);
+const flor=Math.floor(c);
+console.log(cell);
+console.log(flor);
+
+// 4. math.random 
+
+console.log(Math.random());
